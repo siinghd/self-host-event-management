9a48255a4f903bacf538fd57c106e55c60d78328
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-01-08 00:03:00 +0530

    Build : built homepage & navbar

--- frontend/components/Overlay.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function overlay() {
-    return (
-        <div className="absolute h-screen w-screen bg-gradient-to-l from-primary"></div>
-    )
-}
--- frontend/components/ui/navigation-menu.tsx
+++ frontend/components/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md gap-2 px-4 py-2 text-md font-medium transition-colors hover:bg-primary hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+  "group inline-flex h-10 w-max items-center justify-center rounded-3xl gap-2 px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-400 hover:dark:bg-secondary hover:dark:text-secondary-foreground hover:text-accent-foreground focus:bg-gray-400 focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
 )
 
 const NavigationMenuTrigger = React.forwardRef<
